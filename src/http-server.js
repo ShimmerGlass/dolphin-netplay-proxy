5f5e6226ef416283320e55ed03b84f0e09961067
Aestek <thib.gilles@gmail.com>
2015-03-14 15:12:02 +0100

    Default ip to user's

--- src/http-server.js
+++ src/http-server.js
@@ -7,7 +7,14 @@ module.exports = function() {
 	var server = http.Server(app);
 
 	app.use(basicAuth("dnp", "BwenJJ4Cfio3AW"));
+
 	app.use(express.static(__dirname + '/../public'));
+	app.set('view engine', 'ejs');
+	app.set('views', __dirname + '/../views');
+
+	app.get('/', function(req, res) {
+		res.render('index', { ip: req.ip })
+	});
 
 	return server;
 };
